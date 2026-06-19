0caf2586c133d8101459d2b765dcd9758d39decc
Julie Cao <juliehongcao@gmail.com>
2026-06-19 14:42:53 -0700

    test: modify workflow-editor to trigger suggest-reviewers CI (many-contributors case)

--- frontend/src/app/workspace/component/workflow-editor/workflow-editor.component.ts
+++ frontend/src/app/workspace/component/workflow-editor/workflow-editor.component.ts
@@ -1,3 +1,4 @@
+// test: trigger suggest-reviewers CI (many-contributors case)
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
